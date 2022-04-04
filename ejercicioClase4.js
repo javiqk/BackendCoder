5565106f3742977510cc9af19df8286dc28159ef
javiqk <javiercucarella@hotmail.com>
2022-04-03 23:00:22 -0300

    clase 4 final

--- ejercicioClase4.js
+++ ejercicioClase4.js
@@ -47,20 +47,56 @@ class Contenedor {
         const data = fs.readFileSync(this.nombreArchivo, 'utf-8');
         return data;
     }
-        deleteById () {
 
+    deleteById(x){
+        let array= [];
+        let e = x;
+        try{
+            let data = fs.readFileSync(this.nombreArchivo,'utf-8');
+            array = JSON.parse(data);
         }
-        deleteAll () {
-    
+        catch{
+            console.log("producto inexistente"); 
+        }
         
+        
+        array.forEach(element => 
+            {
+           
+                if(element.id == e)
+                {
+                    let id = element.id - 1
+                    let removed = array.splice(id, 1);
+                    console.log("Se eliminó el siguiente elemento: " + JSON.stringify(removed));
+                    fs.writeFileSync(this.nombreArchivo,JSON.stringify(array))  
+                    console.log(array);  
+                }
+            });
+    }
+
+    deleteAll(){
+        let array= []
+        try{
+            let data = fs.readFileSync(this.nombreArchivo,'utf-8');
+            array = JSON.parse(data);
         }
+        catch{
+            console.log("productos inexistentes"); 
+        }
+        array =[]
+        console.log(array);
+        fs.writeFileSync(this.nombreArchivo,JSON.stringify(array));
+    }
     }
 
 
 let container = new Contenedor();
-console.log(container.getById(3));
-console.log(container.getAll());
 
 container.save('Heladera', 1200);
 container.save('Lavarropas', 7000);
 container.save('Televisor', 12000);
+
+container.getById(2);
+container.getAll();
+container.deleteById(1);
+container.deletAll();
